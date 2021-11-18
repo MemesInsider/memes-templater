b3e6fff7f249914e5119127b2e228f0e05e4ac94
Aditya Gupta <ag15035@gmail.com>
2021-11-19 01:33:38 +0530

    Done click issues

--- src/App.jsx
+++ src/App.jsx
@@ -13,94 +13,32 @@ function App() {
     const [bottomText, setBottomText] = useState("");
     const [meme, setMeme] = useState(null);
 
+    const limit_memes = 50; /**How many memes to show? (Max 100) */
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes").then(x =>
-            x.json().then(response => setTemplates(response.data.memes))
+            x.json().then(response => setTemplates(response.data.memes.slice(0, limit_memes)))
         );
     }, []);
 
-    if (meme) {
-        return (
-            <div style={{ textAlign: "center" }}>
-                <img style={{ width: 200 }} src={meme} alt="custom meme" />
-            </div>
-        );
-    }
-
     const total_styles = 6;
-    const sources = [
-        { src: "images/pic01.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic02.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic03.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic04.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic05.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic06.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic07.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic08.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic09.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic10.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic11.jpg", title: "Magna", text: "Something something" },
-        { src: "images/pic12.jpg", title: "Magna", text: "Something something" },
-    ];
 
     return (
         <section className="tiles">
             {
-                templates.map((obj, _idx) => {
-                    const style_idx = Math.ceil(Math.random() * (total_styles));
-
-                    return (
-                        <article id={obj.id} className={""/*`style${style_idx}`*/}>
-                            <span className="image">
-                                <img src={obj.url} alt={obj.name} />
-                            </span>
-                            <a href="generic.html">
-                                <h2>{obj.name}</h2>
-                                <div className="content">
-                                    <p>"Click to use this template !"</p>
-                                </div>
-                            </a>
-                        </article>
-                    )
-                })
+                meme && (
+                    <Meme template={{ url: meme }} no_text={true} />
+                )
             }
-        </section>
-    );
-}
-
-function App2() {
-    const [templates, setTemplates] = useState([]);
-    const [template, setTemplate] = useState(null);
-    const [topText, setTopText] = useState("");
-    const [bottomText, setBottomText] = useState("");
-    const [meme, setMeme] = useState(null);
-
-    useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes").then(x =>
-            x.json().then(response => setTemplates(response.data.memes))
-        );
-    }, []);
-
-    if (meme) {
-        return (
-            <div style={{ textAlign: "center" }}>
-                <img style={{ width: 200 }} src={meme} alt="custom meme" />
-            </div>
-        );
-    }
-    return (
-        <div style={{ textAlign: "center" }}>
-            {template && (
+            {!meme && template && (
                 <form
                     onSubmit={async e => {
                         e.preventDefault();
-                        // add logic to create meme from api
                         const params = {
                             template_id: template.id,
                             text0: topText,
                             text1: bottomText,
-                            username: "xzk03017",
-                            password: "xzk03017@cndps.com"
+                            username: process.env.REACT_APP_IMGFLIP_USERNAME,
+                            password: process.env.REACT_APP_IMGFLIP_PASSWORD
                         };
                         const response = await fetch(
                             `https://api.imgflip.com/caption_image${objectToQueryParam(
@@ -111,36 +49,36 @@ function App2() {
                         setMeme(json.data.url);
                     }}
                 >
-                    <Meme template={template} />
+                    <Meme template={template} styling_class="" onClick={() => {
+                        setTemplate(template);
+                    }} no_text={true} />
                     <input
-                        placeholder="top text"
+                        placeholder="Text at Top"
                         value={topText}
                         onChange={e => setTopText(e.target.value)}
                     />
                     <input
-                        placeholder="bottom text"
+                        placeholder="Text at Bottom"
                         value={bottomText}
                         onChange={e => setBottomText(e.target.value)}
                     />
-                    <button type="submit">create meme</button>
+                    <button type="submit">Create Meme !</button>
                 </form>
             )}
-            {!template && (
-                <>
-                    <h1>Pick a template</h1>
-                    {templates.map(template => {
-                        return (
-                            <Meme
-                                template={template}
-                                onClick={() => {
-                                    setTemplate(template);
-                                }}
-                            />
-                        );
-                    })}
-                </>
-            )}
-        </div>
+            {
+                !meme && !template && templates.map((obj, _idx) => {
+                    const style_idx = Math.ceil(Math.random() * (total_styles));
+
+                    console.log("Here");
+                    return (
+                        <Meme key={_idx} template={obj} styling_class={""/*`style${style_idx}`*/} click_handler={() => {
+                            console.log("Clicked: ", obj);
+                            setTemplate(obj);
+                        }} />
+                    )
+                })
+            }
+        </section>
     );
 }
 
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-export const Meme = ({ template, onClick }) => {
+export const Meme = ({ template, styling_class, click_handler, no_text = false }) => {
   return (
-    <img
-      style={{ width: 200 }}
-      key={template.id}
-      src={template.url}
-      alt={template.name}
-      onClick={onClick}
-    />
+    <article id={template.id} style={{ cursor: "pointer" }} className={styling_class}>
+      <span className="image" style={{ cursor: "pointer" }} onClick={click_handler}>
+        <img src={template.url} alt={template.name} />
+      </span>
+      <a onClick={click_handler}>
+        <h2>{no_text ? "": template.name}</h2>
+        <div className="content" style={{ cursor: "pointer" }} onClick={click_handler}>
+          <p>{no_text ? "": "Click to use this template !"}</p>
+        </div>
+      </a>
+    </article>
   );
 };
